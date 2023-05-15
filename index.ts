fa3d7242601cb7b41f5cf35bddec3eaba68bdb2a
Felipe Parra <fpr29.10@gmail.com>
2023-05-15 03:09:05 -0600

    Initialize

---
+++ index.ts
@@ -0,0 +1,14 @@
+// import state from './state'
+// import * as mutations from './mutations'
+// import * as getters from './getters'
+// import * as actions from './actions'
+
+// const myCustomModule = {
+//   namespaced: true,
+//   state,
+//   mutations,
+//   getters,
+//   actions
+// }
+
+// export default myCustomModule
